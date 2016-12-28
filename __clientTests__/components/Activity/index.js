0973737a6cd1ae4c9ec891367454f2f4515a6b32
Chā <chavis.clark@gmail.com>
2016-12-27 23:15:56 -0500

    wrote failing test for Activity component

---
+++ __clientTests__/components/Activity/index.js
@@ -0,0 +1,24 @@
+import { renderComponent, expect } from '../../test_helper';
+import Activity from '../../../app/components/Activity';
+import classNames from 'classnames/bind';
+import styles from '../../../app/components/Activity/activity.css';
+
+const cx = classNames.bind(styles);
+
+describe('Activity', () => {
+  let component;
+  let props;
+
+  beforeEach(()=> {
+    props = {thisOuting: {text: "Sample"}}
+    component = renderComponent(Activity, props);
+  });
+
+  it('has a correct class', () => {
+    expect(component).to.have.class(cx('activity_container'));
+  });
+
+  it('has a primary activity icon', () => {
+    expect(component.find('div')).to.have.class(cx('activity_icon'));
+  });
+});
